07e4ab615604887ea850cc5d3235ae2630a1b7cc
Jason Lane <quizathaderat@yahoo.com>
2018-10-25 23:13:51 -0700

    Remove commented code and change uri

--- src/client/client.js
+++ src/client/client.js
@@ -1,17 +1,9 @@
 import ApolloClient from "apollo-boost";
-// import defaults from "./defaults"
-// import resolvers from "./resolvers"
-// import typeDefs from "./typeDefs"
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const client = new ApolloClient({
-    uri: "http://localhost:4466",
+    uri: "http://localhost:4000",
     cache: new InMemoryCache()
-    // clientState: {
-    //     defaults,
-    //     resolvers,
-    //     typeDefs,
-    // }
 });
 
 export default client;
